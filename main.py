1d69a0ac7fbe49fb34fcbf4cbd249f6bf73d6544
Messias <messiasfuryforever@gmail.com>
2023-02-04 15:04:34 -0300

    dice-roller-api

---
+++ main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+import uvicorn
+import random
+
+app = FastAPI()
+
+@app.get("/{dice_input}")
+async def main(dice_input: str):
+    dice_split = []
+    dices_mod = []
+    dices = []
+    dices_dict = {}
+    if '+' in dice_input:
+        dice_split = dice_input.split('+')
+    else:
+        dice_split = [dice_input]
+    
+    for dice_verificacao in dice_split:
+        try:
+            dices_mod.append(int(dice_verificacao)) 
+        except ValueError:
+            dices.append(dice_verificacao)
+
+    for dices_verificacao in range(0, len(dices)):
+        try:
+            dice_d_split = dices[dices_verificacao].split('d')
+            dices_amount = int(dice_d_split[0])
+            dices_sides = int(dice_d_split[1])
+            dices_array = []
+            dice_mod = []
+            mod_sum = 0
+            for i in range(dices_amount):
+                dices_array.append(random.randint(1,dices_sides))
+            try:
+                dice_mod = [dices_mod[dices_verificacao]]
+                mod_sum = sum(dice_mod)
+            except:
+                pass
+            greater = max(dices_array)
+            less = min(dices_array)
+            dice_sum = sum(dices_array)
+            dices_dict[dices_verificacao] = {'Amount': dices_amount, 'Sides': dices_sides, 'Results': dices_array, 'Mod': dice_mod, 'ModSum': mod_sum, 'TotalNoMod': dice_sum, 'TotalMod': dice_sum+mod_sum, 'Greater': greater, 'Less': less, 'Average': greater/less, 'GreaterWMod': greater+mod_sum}
+        except ValueError:
+            dices_dict = {"Error": "Invalid input", "0" : {'Amount': 0, 'Sides': 0, 'Results': [], 'Mod': [], 'ModSum': 0, 'TotalNoMod': 0, 'TotalMod': 0, 'Greater': 0, 'Less': 0, 'Average': 0, 'GreaterWMod': 0}}
+    return dices_dict
